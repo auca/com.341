1ac4fb8d289efeed0639d89649b4d68ca9136dd8
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2017-10-19 12:59:00 +0600

    Fix the download URL

--- Practice 1/debian/arm64/utilities/download_debian.sh
+++ Practice 1/debian/arm64/utilities/download_debian.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 DEBIAN='debian-9.2.1-arm64-netinst.iso'
-URL="http://mirrors.kernel.org/debian-cd/9.2.0/arm64/iso-cd/$DEBIAN"
+URL="http://mirrors.kernel.org/debian-cd/9.2.1/arm64/iso-cd/$DEBIAN"
 
 curl -C - -o "$DEBIAN" "$URL" || wget --continue -o "$DEBIAN" "$URL"
