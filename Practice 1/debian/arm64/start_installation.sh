86ce8a557a5a79c1c7e78f802d58855b805db31a
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2018-09-26 17:16:56 +0600

    Fix the Debian version in setup scripts

--- Practice 1/debian/arm64/start_installation.sh
+++ Practice 1/debian/arm64/start_installation.sh
@@ -10,9 +10,9 @@ qemu-system-aarch64                                                         \
     $DISPLAY                                                                \
     -drive if=pflash,file=flash0.img,format=raw                             \
     -drive if=pflash,file=flash1.img,format=raw                             \
-    -drive if=none,file=debian-9.2.1-arm64-hd.qcow2,id=hd0                  \
+    -drive if=none,file=debian-9.5.0-arm64-hd.qcow2,id=hd0                  \
     -device virtio-blk-device,drive=hd0                                     \
-    -drive if=none,file=debian-9.2.1-arm64-netinst.iso,media=cdrom,id=cdrom \
+    -drive if=none,file=debian-9.5.0-arm64-netinst.iso,media=cdrom,id=cdrom \
     -device virtio-scsi-device                                              \
     -device scsi-cd,drive=cdrom                                             \
     -netdev user,hostfwd=tcp::2222-:22,id=eth0                              \
