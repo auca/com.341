4d8b6964ace2c45ab0d155406bd0ed4cd90fc21e
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2018-09-26 16:58:50 +0600

    Bump Debian version to 9.5.0 for amd64

--- Practice 1/debian/amd64/utilities/download_debian.sh
+++ Practice 1/debian/amd64/utilities/download_debian.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-DEBIAN='debian-9.2.0-amd64-netinst.iso'
-URL="http://mirrors.kernel.org/debian-cd/9.2.0/amd64/iso-cd/$DEBIAN"
+DEBIAN='debian-9.5.0-amd64-netinst.iso'
+URL="http://mirrors.kernel.org/debian-cd/9.5.0/amd64/iso-cd/$DEBIAN"
 
 curl -C - -o "$DEBIAN" "$URL" || wget --continue -o "$DEBIAN" "$URL"
